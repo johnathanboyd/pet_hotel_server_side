4269c95bf5102aff0433166a034cd8dc5ac67495
Johnathan <johnathanboyd5@gmail.com>
2021-05-05 15:07:17 -0500

    added queries to populate with test data

--- database.sql
+++ database.sql
@@ -18,3 +18,13 @@ CREATE TABLE "pets" (
 INSERT INTO "pets" (pet_name, owner_name, breed, color, checked_in) VALUES ( %s, %s, %s, %s, %s);
 
 INSERT INTO "owner" (name, "#_of_pets") VALUES ( %s, %s );
+
+-- test data 
+
+INSERT INTO "pets" (pet_name, owner_name, breed, color, checked_in) VALUES 
+('Charlie', 'Chris', 'Shih-tzu', 'Black', false ), 
+('Thorin', 'Chris', 'Rabbit', 'White', false), 
+('Gatsby', 'Ally', 'Cat', 'White', true),
+('Juniper', 'Dane', 'Cat', 'Tabby', false);
+
+INSERT INTO "owner" (name, "#_of_pets") VALUES ( 'Chris', 2), ('Ally', 1), ('Dane', 1);
